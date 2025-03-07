930879bc72cda8550d3db1574c11d7c6a485b579
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-03-07 16:30:54 +0100

    Testcalulator 3

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,8 +1,14 @@
 package se.lexicon;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("My calculator app");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("Choose operation: ");
+        //Scanner ...
 
     double num1 = 15, num2= 6, result = 0;
     char operator = '+';
