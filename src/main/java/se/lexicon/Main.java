17ceba7273c7e2faacb1ef551e6b5f3e608d4d35
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-03-07 15:21:58 +0100

    First testcalulator

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,30 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("My calculator app");
+
+    double num1 = 5, num2= 7, result = 0;
+    char operator = '+';
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            System.out.println(operator + " is not supported");
     }
+        System.out.println(result);
+
+    }
+
+
 }
