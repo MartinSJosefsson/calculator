d871339a73831b6951842cb1e1f210ecb9db1341
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-03-12 15:08:25 +0100

    First commit

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -4,14 +4,65 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My calculator app");
-        System.out.println("1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("Choose operation: ");
-        //Scanner ...
 
-    double num1 = 15, num2= 6, result = 0;
-    char operator = '+';
+        Scanner scanner = new Scanner(System.in);
+        calculator calculator = new calculator();
+        String input = "";
+
+        while (true) {
+            System.out.println("Welcome to my calculator app");
+            System.out.println("Enter first value: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.println("Enter calculation method (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+
+            System.out.println("Enter second value: ");
+            double num2 = scanner.nextDouble();
+
+            double result = 0;
+            boolean validOperation = true;
+
+
+            switch (operator) {
+                case '+':
+                    result = calculator.add(num1, num2);
+                    break;
+                case '-':
+                    result = calculator.subtract(num1, num2);
+                    break;
+                case '*':
+                    result = calculator.multiply(num1, num2);
+                    break;
+                case '/':
+                    try {
+                        result = calculator.divide(num1, num2);
+                    } catch (ArithmeticException e) {
+                        System.out.println(e.getMessage());
+                        validOperation = false;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator! Please use one of the following: +, -, *, /");
+                    validOperation = false;
+                    break;
+            }
+
+            input = scanner.nextLine();
+
+            if (validOperation) {
+                System.out.println("Result: " + result);
+            }
+
+            System.out.println("Do you want to continue? (y/n): ");
+            input = scanner.nextLine();
+            if(input.equalsIgnoreCase("n")) {
+                System.out.println("Thank you for trying my calculator!");
+                break;
+
+
+        }
+
 
     switch (operator) {
         case '+':
@@ -29,9 +80,8 @@ public class Main {
         default:
             System.out.println(operator + " is not supported");
     }
-        System.out.println(result);
+
 
     }
 
-
-}
+}}
